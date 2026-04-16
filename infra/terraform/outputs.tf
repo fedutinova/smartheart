1a79cf23c5b73f8626b775cbe9b28ee3d941c32e
Anna Fedutinova <annafedutinova@gmail.com>
2026-04-16 10:44:01 +0300

    added ipv6

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -21,11 +21,15 @@ output "ssh_command" {
 output "dns_instructions" {
   description = "DNS records to create at your registrar"
   value       = <<-EOT
-    Create these DNS A records at your domain registrar:
+    Create these DNS records at your domain registrar:
 
-      ${var.domain_name}       A  ${hcloud_server.smartheart.ipv4_address}
-      www.${var.domain_name}   A  ${hcloud_server.smartheart.ipv4_address}
-      api.${var.domain_name}   A  ${hcloud_server.smartheart.ipv4_address}
+      ${var.domain_name}       A     ${hcloud_server.smartheart.ipv4_address}
+      www.${var.domain_name}   A     ${hcloud_server.smartheart.ipv4_address}
+      api.${var.domain_name}   A     ${hcloud_server.smartheart.ipv4_address}
+
+      ${var.domain_name}       AAAA  ${hcloud_server.smartheart.ipv6_address}
+      www.${var.domain_name}   AAAA  ${hcloud_server.smartheart.ipv6_address}
+      api.${var.domain_name}   AAAA  ${hcloud_server.smartheart.ipv6_address}
 
     TTL: 300 (or lower for first deploy, raise to 3600 after stable)
   EOT
